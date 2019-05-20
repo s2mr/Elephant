017848d422cdc4205b5a42f123ebcf76b6a34125
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2019-05-20 14:07:42 +0900

    Project init

---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by 下村 一将 on 2019/05/20.
+//  Copyright © 2019 kazuringo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
