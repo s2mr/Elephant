171a5e441ed5d0cba2c6d1ca25192a1139a31c13
Alex Sobol <Maxvit-5@tut.by>
2020-10-20 15:08:08 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(targets: ["Elephant"]),
+        .library(name: "Elephant", targets: ["Elephant"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(dependencies: []),
-        .testTarget(dependencies: ["Elephant"]),
+        .target(name: "Elephant", path: "Sources"),
+        .testTarget(name: "Elephant", dependencies: ["Elephant"]),
     ]
 )
